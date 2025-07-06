58ebabd6f08b18a1872299b856d9155dc677fe02
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-07-06 13:52:27 +0200

    feat(suspicion)!: simplify suspicion api

--- src/server/ai/suspicion/SuspicionManagement.lua
+++ src/server/ai/suspicion/SuspicionManagement.lua
@@ -1,27 +1,21 @@
 --!strict
 
-local SUSPICION_STATES = {
-	CALM = "CALM",
-	SUSPICIOUS = "SUSPICIOUS",
-	ALERTED = "ALERTED"
-}
-
 local CONFIG = {
 	MAX_SUSPICION = 1.0,
-	SUSPICIOUS_THRESHOLD = 0.3,   -- Becomes SUSPICIOUS at 30%
-	ALERTED_THRESHOLD = 1.0,      -- Becomes ALERTED at 100%
-	
 	SUSPICION_DECAY_RATE = 0.05,  -- How fast suspicion decreases per update
 	SUSPICION_RAISE_RATE = 0.07,  -- Base rate for raising suspicion
 }
 
+--[=[
+	@class SuspicionManagement
+
+	Manages the raising and lowering of suspicion per player.
+]=]
 local SuspicionManagement = {}
 SuspicionManagement.__index = SuspicionManagement
 
 export type SuspicionManagement = typeof(setmetatable({} :: {
-	suspicionLevels: { [Player]: number },
-	currentState: "CALM" | "SUSPICIOUS" | "ALERTED",
-	focusedSuspect: Player?
+	suspicionLevels: { [Player]: number }
 }, SuspicionManagement))
 
 local function getSuspectSuspicionWeight(suspect: Player): number
@@ -30,20 +24,18 @@ end
 
 function SuspicionManagement.new(): SuspicionManagement
 	return setmetatable({
-		suspicionLevels = {},
-		currentState = SUSPICION_STATES.CALM,
-		focusedSuspect = nil :: Player?
+		suspicionLevels = {}
 	}, SuspicionManagement)
 end
 
-function SuspicionManagement.increaseSuspicionOnSuspect(self: SuspicionManagement, suspect: Player): ()
+function SuspicionManagement.increaseSuspicion(self: SuspicionManagement, suspect: Player): ()
 	local suspicionWeight = getSuspectSuspicionWeight(suspect)
 	local increase = CONFIG.SUSPICION_RAISE_RATE * suspicionWeight
 	local currentSuspicion = self.suspicionLevels[suspect] or 0.0
 	self.suspicionLevels[suspect] = math.min(CONFIG.MAX_SUSPICION, currentSuspicion + increase)
 end
 
-function SuspicionManagement.lowerSuspicionOnSuspect(self: SuspicionManagement, suspect: Player)
+function SuspicionManagement.lowerSuspicion(self: SuspicionManagement, suspect: Player): ()
 	local currentSuspicion = self.suspicionLevels[suspect] or 0.0
 	
 	if currentSuspicion > 0 then
@@ -53,15 +45,14 @@ end
 
 function SuspicionManagement.update(self: SuspicionManagement): ()
 	for _, player in ipairs(game:GetService("Players"):GetPlayers()) do
-		--warn(player.Character.Raise.Value)
 		if not player.Character then
 			continue
 		end
 
 		if (player.Character:WaitForChild("Raise") :: BoolValue).Value then
-			self:increaseSuspicionOnSuspect(player)
+			self:increaseSuspicion(player)
 		else
-			self:lowerSuspicionOnSuspect(player)
+			self:lowerSuspicion(player)
 		end
 	end
 
