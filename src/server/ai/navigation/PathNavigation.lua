ff8be0f1ea37fe35646b4732ecccad36165348fd
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-07-07 23:50:52 +0200

    feat(navigation): add path navigation and post

---
+++ src/server/ai/navigation/PathNavigation.lua
@@ -0,0 +1,94 @@
+--!nonstrict
+
+local PathfindingService = game:GetService("PathfindingService")
+
+local PathNavigation = {}
+PathNavigation.__index = PathNavigation
+
+export type AgentParameters = {
+	AgentRadius: number, -- these 2 values are useful so the agent wont get stuck in tight corners
+	AgentHeight: number,
+	AgentCanJump: boolean, -- due to the nature of our games, these 2 values are not necessary, leave them as false
+	AgentCanClimb: boolean,
+	WaypointSpacing: number,
+	Costs: {any}
+}
+
+export type PathNavigation = typeof(setmetatable({} :: {
+	character: Model,
+	path: Path?,
+	pathAgentParams: AgentParameters?,
+	waypoints: { PathWaypoint },
+	currentWaypointIndex: number,
+	humanoidMoveToFinishedConnection: RBXScriptConnection?
+}, PathNavigation))
+
+function PathNavigation.new(character: Model): PathNavigation
+	return setmetatable({
+		character = character,
+		path = nil :: Path?,
+		pathAgentParams = nil :: AgentParameters?,
+		waypoints = {},
+		currentWaypointIndex = 1,
+		humanoidMoveToFinishedConnection = nil :: RBXScriptConnection?
+	}, PathNavigation)
+end
+
+function PathNavigation.createPath(self: PathNavigation, toPos: Vector3): Path
+	-- pathfinding service in roblox is very weird.
+	-- a "path" is just a configured class that we use to compute and also
+	-- get the waypoints.
+
+	local path = PathfindingService:CreatePath(self.pathAgentParams)
+	path:ComputeAsync(self.character.PrimaryPart.Position, toPos)
+	local waypoints = path:GetWaypoints()
+
+	self.path = path
+	self.waypoints = waypoints
+	self.currentWaypointIndex = 1
+
+	return path
+end
+
+function PathNavigation.disconnectMoveToConnection(self: PathNavigation): ()
+	local connection = self.humanoidMoveToFinishedConnection
+	if connection then
+		connection:Disconnect()
+		self.humanoidMoveToFinishedConnection = nil
+	end
+end
+
+function PathNavigation.moveTo(self: PathNavigation, toPos: Vector3): ()
+	-- reset for good measure (remove if causing performance problems)
+	-- for now, we dont implement a blocked or stuck handling
+	self:createPath(toPos)
+	self:disconnectMoveToConnection()
+	self.humanoidMoveToFinishedConnection = self.character.Humanoid.MoveToFinished:Connect(function()
+		self:onMoveToFinished()
+	end)
+
+	self.character.Humanoid:MoveTo(self.waypoints[1].Position)
+end
+
+function PathNavigation.onMoveToFinished(self: PathNavigation): ()
+	local currentWaypointIndex = self.currentWaypointIndex
+	print(currentWaypointIndex)
+	local waypoints = self.waypoints
+	if currentWaypointIndex >= #waypoints then
+		self:disconnectMoveToConnection()
+		return
+	end
+
+	self.currentWaypointIndex += 1
+	self.character.Humanoid:MoveTo(waypoints[currentWaypointIndex].Position)
+end
+
+function PathNavigation.stop(self: PathNavigation)
+	self.path = nil
+	self:disconnectMoveToConnection()
+
+	-- move to its current position to stop moving
+	self.character.humanoid:MoveTo(self.character.PrimaryPart.Position)
+end
+
+return PathNavigation
