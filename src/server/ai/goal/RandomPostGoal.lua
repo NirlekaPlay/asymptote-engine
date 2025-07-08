bc0cbe30d69b541cd2c9af0c07ad5cf6c5a3b4b2
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-07-08 13:57:48 +0200

    fix(patrol)!: fix several bugs with interruption

--- src/server/ai/goal/RandomPostGoal.lua
+++ src/server/ai/goal/RandomPostGoal.lua
@@ -9,18 +9,46 @@ RandomPostGoal.__index = RandomPostGoal
 export type RandomPostGoal = typeof(setmetatable({} :: {
 	agent: any,
 	state: "UNEMPLOYED" | "WALKING" | "STAYING",
-	currentPost: GuardPost.GuardPost,
-	releaseUntil: number,
-	posts: {GuardPost}
+	targetPost: GuardPost?,
+	timeToReleasePost: number,
+	posts: {GuardPost},
+	isAtTargetPost: boolean,
 }, RandomPostGoal)) & Goal.Goal
 
-function RandomPostGoal.new(agent, posts: {GuardPost.GuardPost}): RandomPostGoal
+type GuardPost = GuardPost.GuardPost
+
+local function getRandomUnoccupiedPost(posts: { GuardPost }): GuardPost?
+	local unoccupied = {}
+
+	for _, post in ipairs(posts) do
+		if not post:isOccupied() then
+			table.insert(unoccupied, post)
+		end
+	end
+
+	if #unoccupied == 0 then
+		return nil
+	end
+
+	return unoccupied[math.random(1, #unoccupied)]
+end
+
+local function walkToPost(self: RandomPostGoal, post: GuardPost): ()
+	post:occupy()
+	self.isAtTargetPost = false
+	self.targetPost = post
+	self.state = "WALKING"
+	self.agent:getNavigation():moveTo(post.cframe.Position)
+end
+
+function RandomPostGoal.new(agent, posts: {GuardPost}): RandomPostGoal
 	return setmetatable({
 		flags = {},
 		agent = agent,
 		state = "UNEMPLOYED",
-		currentPost = nil,
-		releaseUntil = 0,
+		targetPost = nil,
+		isAtTargetPost = false,
+		timeToReleasePost = 0,
 		posts = posts
 	}, RandomPostGoal)
 end
@@ -43,18 +71,22 @@ function RandomPostGoal.getFlags(self: RandomPostGoal): {Flag}
 end
 
 function RandomPostGoal.start(self: RandomPostGoal): ()
-	warn("Attempt to find post...")
-	local post = self:getRandomUnoccupiedPost()
-	if not post then
-		warn("No unoccupied posts found")
-		return
-	end
+	if self.targetPost and self.targetPost:isOccupied() then
+		if not self.isAtTargetPost then
+			walkToPost(self, self.targetPost)
+		else
+			self.agent:getBodyRotationControl():setRotateTowards(self.targetPost.cframe.LookVector)
+		end
+	else
+		warn("Attempt to find post...")
+		local post = getRandomUnoccupiedPost(self.posts)
+		if not post then
+			warn("No unoccupied posts found")
+			return
+		end
 
-	warn("Post found.")
-	post:occupy()
-	self.currentPost = post
-	self.state = "WALKING"
-	self.agent:getNavigation():moveTo(post.cframe.Position)
+		walkToPost(self, post)
+	end
 end
 
 function RandomPostGoal.stop(self: RandomPostGoal): ()
@@ -70,16 +102,18 @@ function RandomPostGoal.update(self: RandomPostGoal, deltaTime: number): ()
 		-- AWFUL HACK ALERT, i shouldnt do this but i SHOULD
 		nav.finished = false
 		self.state = "STAYING"
-		self.releaseUntil = tick() + math.random(4, 7)
-		rot:setRotateTowards(self.currentPost.cframe.LookVector)
-		warn("Walk to finished. Staying for...", math.floor(self.releaseUntil - tick()), "seconds")
+		self.isAtTargetPost = true
+		self.timeToReleasePost = math.random(4, 7)
+		rot:setRotateTowards(self.targetPost.cframe.LookVector)
+		warn("Walk to finished. Staying for...", self.timeToReleasePost, "seconds")
 	elseif self.state == "STAYING" then
 		--local remaining = self.releaseUntil - tick()
 		--print(`Still waiting at post for {math.ceil(remaining)}s...`)
-		if tick() > self.releaseUntil then
+		self.timeToReleasePost -= deltaTime
+		if self.timeToReleasePost <= 0 then
 			self.state = "UNEMPLOYED"
-			self.currentPost:vacate()
-			self.currentPost = nil
+			self.targetPost:vacate()
+			self.targetPost = nil
 			rot:setRotateTowards(nil)
 			nav:stop()
 		end
@@ -92,22 +126,6 @@ end
 
 function RandomPostGoal.requiresUpdating(self: RandomPostGoal): boolean
 	return true
-end
-
-function RandomPostGoal.getRandomUnoccupiedPost(self: RandomPostGoal): GuardPost.GuardPost
-	local unoccupied = {}
-
-	for _, post in ipairs(self.posts) do
-		if not post:isOccupied() then
-			table.insert(unoccupied, post)
-		end
-	end
-
-	if #unoccupied == 0 then
-		return nil
-	end
-
-	return unoccupied[math.random(1, #unoccupied)]
 end
 
 return RandomPostGoal
