571d1b70e6b0d2cea34c23e1ffe92dc7e6aae9de
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-07-08 09:32:52 +0200

    feat(patrol): encapsulate patrol to goal system

--- src/server/Server.server.lua
+++ src/server/Server.server.lua
@@ -2,6 +2,8 @@ local CollectionService = game:GetService("CollectionService")
 local RunService = game:GetService("RunService")
 local ServerScriptService = game:GetService("ServerScriptService")
 local BodyRotationControl = require(ServerScriptService.server.ai.control.BodyRotationControl)
+local GoalSelector = require(ServerScriptService.server.ai.goal.GoalSelector)
+local RandomPostGoal = require(ServerScriptService.server.ai.goal.RandomPostGoal)
 local GuardPost = require(ServerScriptService.server.ai.navigation.GuardPost)
 local PathNavigation = require(ServerScriptService.server.ai.navigation.PathNavigation)
 local TargetNearbySensor = require(ServerScriptService.server.ai.sensing.TargetNearbySensor)
@@ -18,12 +20,21 @@ local currentNearbySensor = TargetNearbySensor.new(20)
 local currentTriggerZone = TriggerZone.fromPart(workspace:WaitForChild("Zone1"))
 local currentBodyRotCtrl = BodyRotationControl.new(rig.HumanoidRootPart)
 local currentPathNav = PathNavigation.new(rig)
+local currentGoalSelector = GoalSelector.new()
 
-local currentPatrolState: "UNEMPLOYED" | "WALKING" | "STAYING" = "UNEMPLOYED"
-local currentPost: GuardPost?
-local currentPathToPost: Path? = nil
-local releaseUntil = 0
 local currentGuardPosts: { GuardPost } = {}
+
+local GuardAgent = {
+	getSuspicionManager = function()
+		return currentSusMan
+	end,
+	getBodyRotationControl = function()
+		return currentBodyRotCtrl
+	end,
+	getNavigation = function()
+		return currentPathNav
+	end
+}
 
 local function setupGuardPosts()
 	for _, post in ipairs(CollectionService:GetTagged(GUARD_POSTS_TAG_NAME)) do
@@ -32,96 +43,14 @@ local function setupGuardPosts()
 	end
 end
 
-local function getRandomUnoccupiedGuardPost(): GuardPost?
-	local unoccupiedPosts: { GuardPost } = {}
-
-	for i, post in ipairs(currentGuardPosts) do
-		if post:isOccupied() then
-			continue
-		end
-
-		table.insert(unoccupiedPosts, post)
-	end
-
-	local size = #unoccupiedPosts
-	if size <= 0 then
-		return nil
-	end
-
-	return unoccupiedPosts[math.random(1, size)]
-end
-
 setupGuardPosts()
+
+currentGoalSelector:addGoal(RandomPostGoal.new(GuardAgent, currentGuardPosts), 1)
 
 RunService.PreAnimation:Connect(function(deltaTime)
 	currentTriggerZone:update()
 	currentNearbySensor:update(rig.PrimaryPart.Position, currentTriggerZone:getPlayersInZone())
 	currentSusMan:update(deltaTime, currentNearbySensor.detectedTargets)
-
-	-- forgive me.
-	-- please work. PLEASE.
-	-- shit it doesnt work.
-	-- OH SHIT IT WORKS YEAHHHHHHHHHHHHHH
-	if currentSusMan.currentState ~= "SUSPICIOUS" and currentSusMan.currentState ~= "ALERTED" then
-		if currentPatrolState == "UNEMPLOYED" and currentPost == nil then
-			warn(`No assigned post for Guard. Current state is {currentPatrolState}, and current post is {currentPost}`)
-			warn("Attempt to find post...")
-			local fetchPost = getRandomUnoccupiedGuardPost()
-			if not fetchPost then
-				warn("No unoccupied posts found")
-				return
-			end
-
-			warn("Post found.")
-			fetchPost:occupy()
-			currentPost = fetchPost
-			currentPatrolState = "WALKING"
-			currentPathNav:moveTo(fetchPost.cframe.Position)
-			currentPathToPost = currentPathNav.path
-		end
-
-		if currentPatrolState == "WALKING" then
-			if currentPathToPost == "nil" then
-				warn("shit happend, currentPathToPost is nil")
-				return
-			end
-
-			--print(currentPathNav.path, currentPathNav.finished)
-			if currentPathNav.path == currentPathToPost and currentPathNav.finished then
-				-- AWFUL HACK ALERT:
-				currentPathNav.finished = false
-				currentPatrolState = "STAYING"
-				releaseUntil = tick() + math.random(4, 7)
-				currentBodyRotCtrl:setRotateTowards(currentPost.cframe.LookVector)
-				warn("Walk to finished. Staying for...", releaseUntil, "seconds")
-			end
-		end
-
-		if currentPatrolState == "STAYING" then
-			--local remaining = releaseUntil - tick()
-			--print(`Still waiting at post for {math.ceil(remaining)}s...`)
-			if tick() > releaseUntil then
-				warn("Staying finished. Vacating...")
-				currentPost:vacate()
-				currentPost = nil
-				currentPatrolState = "UNEMPLOYED"
-				currentBodyRotCtrl:setRotateTowards(nil)
-			end
-		end
-	else
-		if currentPost then
-			warn("Path interrupted, targeted suspect is", currentSusMan.focusingSuspect)
-			warn("Vacating...")
-			currentPost:vacate()
-			currentPost = nil
-			currentPatrolState = "UNEMPLOYED"
-			currentPathNav:stop()
-		end
-		if currentSusMan.currentState == "SUSPICIOUS" then
-		currentBodyRotCtrl:setRotateTowards(currentSusMan.focusingSuspect.Character.PrimaryPart.Position)
-		else
-			currentBodyRotCtrl:setRotateTowards(nil)
-		end
-		currentBodyRotCtrl:update(deltaTime)
-	end
+	currentGoalSelector:update(deltaTime)
+	
 end)
---
+++ src/server/ai/goal/RandomPostGoal.lua
@@ -0,0 +1,113 @@
+--!nonstrict
+
+local GuardPost = require("../navigation/GuardPost")
+local Goal = require("./Goal")
+
+local RandomPostGoal = {}
+RandomPostGoal.__index = RandomPostGoal
+
+export type RandomPostGoal = typeof(setmetatable({} :: {
+	agent: any,
+	state: "UNEMPLOYED" | "WALKING" | "STAYING",
+	currentPost: GuardPost.GuardPost,
+	releaseUntil: number,
+	posts: {GuardPost}
+}, RandomPostGoal)) & Goal.Goal
+
+function RandomPostGoal.new(agent, posts: {GuardPost.GuardPost}): RandomPostGoal
+	return setmetatable({
+		flags = {},
+		agent = agent,
+		state = "UNEMPLOYED",
+		currentPost = nil,
+		releaseUntil = 0,
+		posts = posts
+	}, RandomPostGoal)
+end
+
+function RandomPostGoal.canUse(self: RandomPostGoal): boolean
+	local susMan = self.agent:getSuspicionManager()
+	return susMan.currentState ~= "SUSPICIOUS" and susMan.currentState ~= "ALERTED"
+end
+
+function RandomPostGoal.canContinueToUse(self: RandomPostGoal): boolean
+	return self:canUse()
+end
+
+function RandomPostGoal.isInterruptable(self: RandomPostGoal): boolean
+	return true
+end
+
+function RandomPostGoal.getFlags(self: RandomPostGoal): {Flag}
+	return self.flags
+end
+
+function RandomPostGoal.start(self: RandomPostGoal): ()
+	warn("Attempt to find post...")
+	local post = self:getRandomUnoccupiedPost()
+	if not post then
+		warn("No unoccupied posts found")
+		return
+	end
+
+	warn("Post found.")
+	post:occupy()
+	self.currentPost = post
+	self.state = "WALKING"
+	self.agent:getNavigation():moveTo(post.cframe.Position)
+end
+
+function RandomPostGoal.stop(self: RandomPostGoal): ()
+	self.agent:getNavigation():stop()
+end
+
+function RandomPostGoal.update(self: RandomPostGoal, deltaTime: number): ()
+	local nav = self.agent:getNavigation()
+	local rot = self.agent:getBodyRotationControl()
+
+	-- WARNING: Might need to check if the path is the same but fuck it
+	if self.state == "WALKING" and nav.finished then
+		-- AWFUL HACK ALERT, i shouldnt do this but i SHOULD
+		nav.finished = false
+		self.state = "STAYING"
+		self.releaseUntil = tick() + math.random(4, 7)
+		rot:setRotateTowards(self.currentPost.cframe.LookVector)
+		warn("Walk to finished. Staying for...", math.floor(self.releaseUntil - tick()), "seconds")
+	elseif self.state == "STAYING" then
+		local remaining = self.releaseUntil - tick()
+		print(`Still waiting at post for {math.ceil(remaining)}s...`)
+		if tick() > self.releaseUntil then
+			self.state = "UNEMPLOYED"
+			self.currentPost:vacate()
+			self.currentPost = nil
+			rot:setRotateTowards(nil)
+			nav:stop()
+		end
+	end
+
+	if self.state == "UNEMPLOYED" then
+		self:start()
+	end
+end
+
+function RandomPostGoal.requiresUpdating(self: RandomPostGoal): boolean
+	return true
+end
+
+function RandomPostGoal.getRandomUnoccupiedPost(self: RandomPostGoal): GuardPost.GuardPost
+	local unoccupied = {}
+
+	for _, post in ipairs(self.posts) do
+		if not post:isOccupied() then
+			table.insert(unoccupied, post)
+		end
+	end
+
+	if #unoccupied == 0 then
+		return nil
+	end
+
+	return unoccupied[math.random(1, #unoccupied)]
+end
+
+return RandomPostGoal
