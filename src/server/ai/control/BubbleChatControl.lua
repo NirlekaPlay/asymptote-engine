026a0bcabb809d31e3063ac98e046722df0b612e
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-07-17 15:22:38 +0200

    feat(control): add BubbleChatControl

---
+++ src/server/ai/control/BubbleChatControl.lua
@@ -0,0 +1,27 @@
+--!strict
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TypedBubbleChatRemote = require(ReplicatedStorage.shared.network.TypedBubbleChatRemote)
+
+--[=[
+	@class BubbleChatControl
+
+	Controls the display of bubble chat to clients.
+]=]
+local BubbleChatControl = {}
+BubbleChatControl.__index = BubbleChatControl
+
+export type BubbleChatControl = typeof(setmetatable({} :: {
+	character: Model
+}, BubbleChatControl))
+
+function BubbleChatControl.new(character: Model): BubbleChatControl
+	return setmetatable({
+		character = character
+	}, BubbleChatControl)
+end
+
+function BubbleChatControl.displayBubble(self: BubbleChatControl, text: string): ()
+    TypedBubbleChatRemote:FireAllClients(self.character.PrimaryPart, text)
+end
+
+return BubbleChatControl
