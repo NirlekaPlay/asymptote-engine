3f11e83d005d9b00641dbc62dd9054eb201c2672
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-08-03 16:36:15 +0200

    refactor: correct naming

--- src/client/PlayerScripts/Status.client.lua
+++ src/client/PlayerScripts/Status.client.lua
@@ -4,7 +4,7 @@ local REMOTE = require(game.ReplicatedStorage.shared.network.TypedRemotes).Statu
 local GUI = game.Players.LocalPlayer.PlayerGui:WaitForChild("Status").Frame1
 
 -- too fucking lazy to port them.
-local ScreenGuies = {
+local PlayerStatus = {
 	MINOR_TRESPASSING = "MINOR_TRESPASSING",
 	MAJOR_TRESPASSING = "MAJOR_TRESPASSING",
 	MINOR_SUSPICIOUS = "MINOR_SUSPICIOUS",
@@ -15,34 +15,33 @@ local ScreenGuies = {
 }
 
 local ScreenGuiTypePerUi = {
-	[ScreenGuies.DISGUISED] = GUI.Frame.A_Disguised,
-	[ScreenGuies.MINOR_TRESPASSING] = GUI.Frame.B_Trespassing,
-	[ScreenGuies.MINOR_SUSPICIOUS] = GUI.Frame.C_Suspicious,
-	[ScreenGuies.MAJOR_TRESPASSING] = GUI.Frame.D_TrespassingRed,
-	[ScreenGuies.CRIMINAL_SUSPICIOUS] = GUI.Frame.E_SuspiciousRed,
-	[ScreenGuies.DANGEROUS_ITEM] = GUI.Frame.F_DangerousItem,
-	[ScreenGuies.ARMED] = GUI.Frame.G_Armed
+	[PlayerStatus.DISGUISED] = GUI.Frame.A_Disguised,
+	[PlayerStatus.MINOR_TRESPASSING] = GUI.Frame.B_Trespassing,
+	[PlayerStatus.MINOR_SUSPICIOUS] = GUI.Frame.C_Suspicious,
+	[PlayerStatus.MAJOR_TRESPASSING] = GUI.Frame.D_TrespassingRed,
+	[PlayerStatus.CRIMINAL_SUSPICIOUS] = GUI.Frame.E_SuspiciousRed,
+	[PlayerStatus.DANGEROUS_ITEM] = GUI.Frame.F_DangerousItem,
+	[PlayerStatus.ARMED] = GUI.Frame.G_Armed
 }
 
-local currentScreenGuies = {}
+local currentPlayerStatus = {}
 
-REMOTE.OnClientEvent:Connect(function(ScreenGuiTypes)
-	print(ScreenGuiTypes)
-	for ScreenGuiType in pairs(ScreenGuiTypes) do
-		if currentScreenGuies[ScreenGuiType] then
+REMOTE.OnClientEvent:Connect(function(playerStatusesMap)
+	for ScreenGuiType in pairs(playerStatusesMap) do
+		if currentPlayerStatus[ScreenGuiType] then
 			continue
 		end
 
-		currentScreenGuies[ScreenGuiType] = true
+		currentPlayerStatus[ScreenGuiType] = true
 		local ui = ScreenGuiTypePerUi[ScreenGuiType]
 		if ui then
 			ui.Visible = true
 		end
 	end
 
-	for ScreenGuiType in pairs(currentScreenGuies) do
-		if not ScreenGuiTypes[ScreenGuiType] then
-			currentScreenGuies[ScreenGuiType] = nil
+	for ScreenGuiType in pairs(currentPlayerStatus) do
+		if not playerStatusesMap[ScreenGuiType] then
+			currentPlayerStatus[ScreenGuiType] = nil
 			local ui = ScreenGuiTypePerUi[ScreenGuiType]
 			if ui then
 				ui.Visible = false
