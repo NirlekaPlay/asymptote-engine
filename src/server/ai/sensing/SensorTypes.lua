ddc8a56f03f5dc19a5b07c1af49862dff29a53f8
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-08-04 14:15:41 +0200

    feat(sensor): add hearing sensor

--- src/server/ai/sensing/SensorTypes.lua
+++ src/server/ai/sensing/SensorTypes.lua
@@ -1,8 +1,10 @@
 --!nonstrict
 
+local HearingPlayersSensor = require(script.Parent.HearingPlayersSensor)
 local SensorType = require(script.Parent.SensorType)
 local VisiblePlayersSensor = require(script.Parent.VisiblePlayersSensor)
 
 return {
-	VISIBLE_PLAYERS_SENSOR = SensorType.new(VisiblePlayersSensor.new)
+	VISIBLE_PLAYERS_SENSOR = SensorType.new(VisiblePlayersSensor.new),
+	HEARING_PLAYERS_SENSOR = SensorType.new(HearingPlayersSensor.new)
 }
