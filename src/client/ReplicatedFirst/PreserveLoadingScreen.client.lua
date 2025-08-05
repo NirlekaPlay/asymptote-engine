78bc788f9240918e3b21cfe3e7c656aee4d819fb
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-08-05 16:07:03 +0200

    feat!: add preserve loading screen script

---
+++ src/client/ReplicatedFirst/PreserveLoadingScreen.client.lua
@@ -0,0 +1,73 @@
+--!strict
+
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local TweenService = game:GetService("TweenService")
+
+local arriveTime = os.clock()
+local loadedTime: number
+
+local MIN_PRESERVE_TIME = 5
+
+local customLoadingScreen = TeleportService:GetArrivingTeleportGui()
+if customLoadingScreen then
+	local playerGui = Players.LocalPlayer:WaitForChild("PlayerGui")
+	ReplicatedFirst:RemoveDefaultLoadingScreen()
+	customLoadingScreen.Parent = playerGui
+else
+	script:Destroy()
+	return
+end
+
+local function animateLoadingScreenFadeAndDestroy()
+	if not customLoadingScreen then
+		return
+	end
+
+	local tweenInfo = TweenInfo.new(3, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
+	local objectsToAnimate: { [GuiBase]: { [string]: any } } = {}
+	for _, guiObject in ipairs(customLoadingScreen:GetDescendants()) do
+		if guiObject:IsA("Frame") then
+			objectsToAnimate[guiObject] = { BackgroundTransparency = 1 }
+		elseif guiObject:IsA("TextLabel") then
+			objectsToAnimate[guiObject] = { TextTransparency = 1 }
+		elseif guiObject:IsA("ImageLabel") then
+			objectsToAnimate[guiObject] = { ImageTransparency = 1, BackgroundTransparency = 1 }
+		end
+	end
+
+	local firstObjectWaitEvent: RBXScriptSignal<Enum.PlaybackState>? = nil
+	for guiObject, properties in pairs(objectsToAnimate) do
+		for property, value in pairs(properties) do
+			local newTween = TweenService:Create(guiObject, tweenInfo, { [property] = value })
+			newTween:Play()
+			if not firstObjectWaitEvent then
+				firstObjectWaitEvent = newTween.Completed
+			end
+		end
+	end
+
+	if firstObjectWaitEvent then
+		firstObjectWaitEvent:Wait()
+	end
+
+	customLoadingScreen:Destroy()
+end
+
+repeat
+	task.wait(1)
+until game:IsLoaded()
+loadedTime = os.clock()
+local timeDifference = loadedTime - arriveTime
+
+if timeDifference > MIN_PRESERVE_TIME then
+	animateLoadingScreenFadeAndDestroy()
+	return
+end
+
+task.wait(MIN_PRESERVE_TIME - timeDifference)
+
+animateLoadingScreenFadeAndDestroy()
+
+script:Destroy()
