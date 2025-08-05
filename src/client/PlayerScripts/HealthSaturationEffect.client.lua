2db256edeb20587b137bde871de6decc291ce735
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-08-05 09:48:54 +0200

    feat: add vignette to health saturation effect

--- src/client/PlayerScripts/HealthSaturationEffect.client.lua
+++ src/client/PlayerScripts/HealthSaturationEffect.client.lua
@@ -8,9 +8,44 @@ colorCorrectionEffect.Parent = currentCamera
 
 local currentCharacter: Model?
 local humanoidHealthChangedConnection: RBXScriptConnection?
+local vignetteScreenGui = localPlayer.PlayerGui:FindFirstChild("Vignette") :: ScreenGui?
 
 if localPlayer.Character then
 	currentCharacter = localPlayer.Character
+end
+
+local function createVignetteGui(): ScreenGui
+	local screenGui = Instance.new("ScreenGui")
+	screenGui.IgnoreGuiInset = true
+	screenGui.DisplayOrder = 10
+	screenGui.ResetOnSpawn = false
+	screenGui.Name = "Vignette"
+
+	local vignetteImage = Instance.new("ImageLabel")
+	vignetteImage.BackgroundTransparency = 1
+	vignetteImage.Size = UDim2.fromScale(1, 1)
+	vignetteImage.ImageContent = Content.fromAssetId(15447075793)
+	vignetteImage.ScaleType = Enum.ScaleType.Stretch
+	vignetteImage.ImageTransparency = 1
+	vignetteImage.Parent = screenGui
+
+	screenGui.Parent = localPlayer.PlayerGui
+
+	return screenGui
+end
+
+if not vignetteScreenGui then
+	vignetteScreenGui = createVignetteGui()
+end
+
+local function updateHealthEffects(currentHealth: number)
+	colorCorrectionEffect.Saturation = math.map(currentHealth, 0, 100, -1, 0)
+	colorCorrectionEffect.Brightness = math.map(currentHealth, 0, 100, -0.3, 0)
+	colorCorrectionEffect.Contrast = math.map(currentHealth, 0, 100, 0.3, 0)
+
+	if not vignetteScreenGui then return end
+	(vignetteScreenGui:FindFirstChild("ImageLabel") :: ImageLabel)
+		.ImageTransparency = math.map(currentHealth, 0, 100, 0, 1)
 end
 
 localPlayer.CharacterAdded:Connect(function(character)
@@ -22,13 +57,9 @@ localPlayer.CharacterAdded:Connect(function(character)
 		humanoidHealthChangedConnection:Disconnect()
 	end
 	
-	colorCorrectionEffect.Saturation = math.map(humanoid.Health, 0, 100, -1, 0)
-	colorCorrectionEffect.Brightness = math.map(humanoid.Health, 0, 100, -0.3, 0)
-	colorCorrectionEffect.Contrast = math.map(humanoid.Health, 0, 100, 0.3, 0)
+	updateHealthEffects(humanoid.Health)
 	humanoidHealthChangedConnection = humanoid.HealthChanged:Connect(function(healthValue)
-		colorCorrectionEffect.Saturation = math.map(healthValue, 0, 100, -1, 0)
-		colorCorrectionEffect.Brightness = math.map(healthValue, 0, 100, -0.3, 0)
-		colorCorrectionEffect.Contrast = math.map(healthValue, 0, 100, 0.3, 0)
+		updateHealthEffects(healthValue)
 	end)
 end)
 
