e483da6636af3f6b36328671b8006cfe92910597
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-08-28 17:50:47 +0200

    feat(level)!: add MissionConfig module to configurate mission

--- src/server/level/Level.lua
+++ src/server/level/Level.lua
@@ -4,30 +4,14 @@ local ServerScriptService = game:GetService("ServerScriptService")
 
 local Cell = require(ServerScriptService.server.cell.Cell)
 local CellConfig = require(ServerScriptService.server.cell.CellConfig)
-local PlayerStatus = require(ServerScriptService.server.player.PlayerStatus)
 
+local HIDE_CELLS = false
 local UPDATES_PER_SEC = 20
 local UPDATE_INTERVAL = 1 / UPDATES_PER_SEC
 local timeAccum = 0
 
-local cellConfigs: {
-	[string]: CellConfig.Config
-} = {
-	KillhouseBasicMinorTrespassing = {
-		canBeTrespassed = true,
-		penalties = {
-			disguised = nil,
-			undisguised = PlayerStatus.Status.MINOR_TRESPASSING
-		}
-	},
-	JeiasZone = {
-		canBeTrespassed = true,
-		penalties = {
-			disguised = PlayerStatus.Status.MINOR_TRESPASSING,
-			undisguised = PlayerStatus.Status.MAJOR_TRESPASSING
-		}
-	}
-}
+local levelFolder: Folder?
+local cellsConfig: { [string]: CellConfig.Config}?
 
 --[=[
 	@class Level
@@ -35,10 +19,17 @@ local cellConfigs: {
 local Level = {}
 
 function Level.initializeLevel(): ()
-	local levelFolder = workspace:FindFirstChild("Level")
+	levelFolder = workspace:FindFirstChild("Level") :: Folder?
 	if not levelFolder or not levelFolder:IsA("Folder") then
 		warn("Unable to initialize Level: Level not found in Workspace or is not a Folder.")
 		return
+	end
+
+	local missionSetupModule = levelFolder:FindFirstChild("MissionSetup") :: ModuleScript?
+	if not missionSetupModule or not missionSetupModule:IsA("ModuleScript") then
+		warn("Unable to initialize Mission: MissionSetup module not found in Level folder or is not a ModuleScript.")
+	else
+		cellsConfig = require(missionSetupModule).Cells
 	end
 
 	local cellsFolder = levelFolder:FindFirstChild("Cells")
@@ -56,19 +47,29 @@ function Level.initializeCells(cellsFolder: Folder): ()
 		end
 
 		local cellName = cellModel.Name
-		local cellConfig = cellConfigs[cellName]
+		local cellConfig = Level.getCellConfig(cellName)
 		local cframe, size = cellModel:GetBoundingBox()
 		local areaName = cellModel:GetAttribute("AreaName") :: string?
 		local bounds = { CFrame = cframe, Size = size, AreaName = areaName }
 		Cell.addCell(cellName, bounds, cellConfig)
 
-		for _, cellChild in ipairs(cellModel:GetChildren()) do
-			if not cellChild:IsA("BasePart") then
-				continue
-			end
-
-			cellChild.Transparency = 1
+		if HIDE_CELLS then
+			Level.hideCell(cellModel)
 		end
+	end
+end
+
+function Level.getCellConfig(cellName: string): CellConfig.Config?
+	return cellsConfig and cellsConfig[cellName] or nil
+end
+
+function Level.hideCell(cellModel: Model): ()
+	for _, cellChild in ipairs(cellModel:GetChildren()) do
+		if not cellChild:IsA("BasePart") then
+			continue
+		end
+
+		cellChild.Transparency = 1
 	end
 end
 
