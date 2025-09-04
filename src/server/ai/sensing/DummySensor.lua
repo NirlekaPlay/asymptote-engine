3151c215a0612e0eae0fbba43157edf30c6c5edf
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-09-04 21:44:56 +0200

    refactor(sensor): update outdated DummySensor

--- src/server/ai/sensing/DummySensor.lua
+++ src/server/ai/sensing/DummySensor.lua
@@ -2,36 +2,35 @@
 
 local ServerScriptService = game:GetService("ServerScriptService")
 
+local Agent = require(ServerScriptService.server.Agent)
+local PerceptiveAgent = require(ServerScriptService.server.PerceptiveAgent)
 local MemoryModuleTypes = require(ServerScriptService.server.ai.memory.MemoryModuleTypes)
 
 local DummySensor = {}
 DummySensor.__index = DummySensor
 
 export type DummySensor = typeof(setmetatable({} :: {
-	_timeAccumulator: number,
 }, DummySensor))
+
+type Agent = Agent.Agent & PerceptiveAgent.PerceptiveAgent -- the agent type idk
 
 function DummySensor.new(): DummySensor
 	return setmetatable({
-		_timeAccumulator = 0
+		rayParams = nil :: RaycastParams?
 	}, DummySensor)
 end
 
-function DummySensor.requires(self: DummySensor): { MemoryModuleTypes.MemoryModuleType<any> }
-	return { MemoryModuleTypes.VISIBLE_PLAYERS }
+function DummySensor.getRequiredMemories(self: DummySensor): { MemoryModuleTypes.MemoryModuleType<any> }
+	return {} -- return all required memories you need
 end
 
-function DummySensor.update(self: DummySensor, agent, deltaTime: number): ()
-	self._timeAccumulator += deltaTime
-
-	if self._timeAccumulator >= 1 then
-		self._timeAccumulator = 0
-		self:doUpdate(agent, deltaTime)
-	end
+function DummySensor.getScanRate(self: DummySensor): number
+	return 1/20 -- scan rate in seconds
 end
 
-function DummySensor.doUpdate(self: DummySensor, agent, deltaTime: number)
-	print("Hello, world! From DummySensor.doUpdate!")
+function DummySensor.doUpdate(self: DummySensor, agent: Agent, deltaTime: number)
+	-- actual logic here
+	-- just type "agent" and you will see all of the numerous methods and properties
 end
 
 return DummySensor
