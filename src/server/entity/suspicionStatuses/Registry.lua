6c283aaa7cd066bfb4027428d58e3951ca2f478d
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-09-06 09:34:45 +0200

    refactor: fix some types

--- src/server/entity/suspicionStatuses/Registry.lua
+++ src/server/entity/suspicionStatuses/Registry.lua
@@ -1,6 +1,5 @@
 --!strict
 local Status = require(script.Parent.Status)
-local Types = require(script.Parent.Types)
 
 return {
 	DISGUISED = Status.new("DISGUISED", 1, true, -37.5),
@@ -9,6 +8,5 @@ return {
 	MAJOR_TRESPASSING = Status.new("MAJOR_TRESPASSING", 4, false, 25),
 	CRIMINAL_SUSPICIOUS = Status.new("CRIMINAL_SUSPICIOUS", 5, true, 25),
 	DANGEROUS_ITEM = Status.new("DANGEROUS_ITEM", 6, true, 50),
-	ARMED = Status.new("ARMED", 7, true, 50),
-
-} :: { [string]: Types.Status }
+	ARMED = Status.new("ARMED", 7, true, 50)
+}
