0121f31c885544656ffb1d6e52ab806f1599e87d
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-10-10 20:43:14 +0200

    feat(chat): add yellow and dark aqua colors

--- src/shared/network/chat/NamedTextColors.lua
+++ src/shared/network/chat/NamedTextColors.lua
@@ -4,5 +4,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TextColor = require(ReplicatedStorage.shared.network.chat.TextColor)
 
 return {
-	RED = TextColor.new(0xF74B52)
+	RED = TextColor.new(0xF74B52),
+	YELLOW = TextColor.new(16777045),
+	DARK_AQUA = TextColor.new(43690)
 }
