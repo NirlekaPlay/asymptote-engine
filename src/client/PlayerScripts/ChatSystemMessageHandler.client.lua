33d05de7923172f04d2689947db676b26f743eb0
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-10-10 19:14:08 +0200

    feat(chat)!: update client system chat to render text components

--- src/client/PlayerScripts/ChatSystemMessageHandler.client.lua
+++ src/client/PlayerScripts/ChatSystemMessageHandler.client.lua
@@ -2,37 +2,16 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TextChatService = game:GetService("TextChatService")
+local MutableTextComponent = require(ReplicatedStorage.shared.network.chat.MutableTextComponent)
 local TypedRemotes = require(ReplicatedStorage.shared.network.remotes.TypedRemotes)
-local RBXGeneral = TextChatService:WaitForChild("TextChannels"):WaitForChild("RBXGeneral") :: TextChannel
 
-local HEX_WHITE = "#ffffff"
-local HEX_RED = "#F52727"
-local DEFAULT_TEXT_COLOR = HEX_WHITE
+local textChannelsFolder = TextChatService:WaitForChild("TextChannels")
+local targetTextChannel = textChannelsFolder:WaitForChild("RBXSystem") :: TextChannel
 
-local function escapeHtml(str: string): string
-	return (str :: any):gsub("<", "&lt;"):gsub(">", "&gt;")
-end
-
-local function displaySystemMessage(str: string, color: string?): ()
-	color = color or DEFAULT_TEXT_COLOR
-	
-	for line in str:gmatch("([^\n]+)") do
-		if color == DEFAULT_TEXT_COLOR then
-			RBXGeneral:DisplaySystemMessage(line)
-		else
-			local escapedLine = escapeHtml(line)
-			local formattedLine = string.format('<font color="%s">%s</font>', color, escapedLine)
-			RBXGeneral:DisplaySystemMessage(formattedLine)
-		end
-	end
+local function displaySystemMessage(str: string, metadata: string?): ()
+	targetTextChannel:DisplaySystemMessage(str, metadata)
 end
 
 TypedRemotes.ClientBoundChatMessage.OnClientEvent:Connect(function(payload)
-	local color: string?
-	if payload.type == "plain" then
-		color = HEX_WHITE
-	elseif payload.type == "error" then
-		color = HEX_RED
-	end
-	displaySystemMessage(payload.literalString, color)
+	displaySystemMessage(MutableTextComponent.deserialize(payload.content):buildRichTextMarkupString())
 end)
