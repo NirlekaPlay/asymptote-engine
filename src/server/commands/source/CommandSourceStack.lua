59a9068c8434d64e8bf77bd868404c132f266043
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-10-10 19:24:52 +0200

    feat(commands)!: add system message handling for CommandSourceStack

--- src/server/commands/source/CommandSourceStack.lua
+++ src/server/commands/source/CommandSourceStack.lua
@@ -1,7 +1,16 @@
 --!strict
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
+local MutableTextComponent = require(ReplicatedStorage.shared.network.chat.MutableTextComponent)
+local NamedTextColors = require(ReplicatedStorage.shared.network.chat.NamedTextColors)
+local TextStyle = require(ReplicatedStorage.shared.network.chat.TextStyle)
 local CommandSource = require(ServerScriptService.server.commands.source.CommandSource)
+
+local FORMATTING_STYLES = {
+	RED = TextStyle.empty()
+		:withColor(NamedTextColors.RED)
+}
 
 --[=[
 	@class CommandSourceStack
@@ -39,6 +48,23 @@ function CommandSourceStack.getPlayerOrThrow(self: CommandSourceStack): Player
 	else
 		error("CommandSourceStack.getPlayerOrThrow() failure: Entity is not of type Player.")
 	end
+end
+
+function CommandSourceStack.sendSuccess(
+	self: CommandSourceStack,
+	component: MutableTextComponent.MutableTextComponent
+): ()
+	self.source:sendSystemMessage(component)
+end
+
+function CommandSourceStack.sendFailure(
+	self: CommandSourceStack,
+	component: MutableTextComponent.MutableTextComponent
+): ()
+	self.source:sendSystemMessage(
+		MutableTextComponent.literal("")
+			:appendComponent(component)
+			:withStyle(FORMATTING_STYLES.RED))
 end
 
 return CommandSourceStack
