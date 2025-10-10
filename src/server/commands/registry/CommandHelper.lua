bd9d1fac5cd4e51df51c1b14fb6a350b2aa63e93
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-10-10 19:24:13 +0200

    refactor(commands)!: fix type errors

--- src/server/commands/registry/CommandHelper.lua
+++ src/server/commands/registry/CommandHelper.lua
@@ -3,7 +3,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
 local ArgumentType = require(ReplicatedStorage.shared.commands.arguments.ArgumentType)
-local ArgumentBuilder = require(ReplicatedStorage.shared.commands.builder.ArgumentBuilder)
 local CommandSourceStack = require(ServerScriptService.server.commands.source.CommandSourceStack)
 local LiteralArgumentBuilder = require(ReplicatedStorage.shared.commands.builder.LiteralArgumentBuilder)
 local RequiredArgumentBuilder = require(ReplicatedStorage.shared.commands.builder.RequiredArgumentBuilder)
@@ -14,8 +13,6 @@ function CommandHelper.literal(literalString: string): LiteralArgumentBuilder.Li
 	return LiteralArgumentBuilder.new(literalString) :: any
 end
 
-function CommandHelper.argument<T>(name: string, argumentType: ArgumentType.ArgumentType<T>): ArgumentBuilder.ArgumentBuilder<CommandSourceStack.CommandSourceStack>
-	return RequiredArgumentBuilder.new(name, argumentType) :: any
-end
+CommandHelper.argument = RequiredArgumentBuilder.new :: <T>(argName: string, argType: ArgumentType.ArgumentType<T>) -> RequiredArgumentBuilder.RequiredArgumentBuilder<CommandSourceStack.CommandSourceStack>
 
 return CommandHelper
