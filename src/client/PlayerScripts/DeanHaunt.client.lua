b169c3813a2c525bb0764f34454435ab0cb704bc
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-10-19 12:32:36 +0200

    feat!: add dean haunting

---
+++ src/client/PlayerScripts/DeanHaunt.client.lua
@@ -0,0 +1,6 @@
+--!strict
+
+local StarterPlayer = game:GetService("StarterPlayer")
+local DeanHaunt = require(StarterPlayer.StarterPlayerScripts.client.modules.character.DeanHaunt)
+
+DeanHaunt.initialize()
---
+++ src/client/PlayerScripts/modules/character/DeanHaunt.lua
@@ -0,0 +1,164 @@
+--!strict
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local BoundingBox = require(ReplicatedStorage.shared.math.geometry.BoundingBox)
+
+local DeanHaunt = {}
+
+local rng = Random.new(os.clock() + tick())
+local CHARACTER = ReplicatedStorage:FindFirstChild("Dean") :: Model
+local SPAWN_CHANCE = 0.001
+local MIN_SPAWN_DISTANCE = 5
+local MAX_SPAWN_DISTANCE = 15
+local DESPAWN_SPEED = 0.3
+local DISAPPEAR_SOUND = ReplicatedStorage.shared.assets.sounds.events.heavy_fast_breathing
+local HANGING_AROUND_SOUND = ReplicatedStorage.shared.assets.sounds.gunsys.heartbeat_96bpm
+local HANGING_AROUND_SOUND_MAX_VOLUME = 1.5
+local HANGING_AROUND_SOUND_RAMP_SPEED = 0.5 -- per sec
+local SPAWN_SOUND = ReplicatedStorage.shared.assets.sounds.events.dean_spawn
+local SPAWN_SOUND_MIN_PLAYBACK_SPEED = 1
+local SPAWN_SOUND_MAX_PLAYBACK_SPEED = 4
+
+local camera = (workspace.CurrentCamera or workspace:FindFirstChildOfClass("Camera")) :: Camera
+local characterToCheck: Model? = nil
+local activeSpook: Model? = nil
+local activeSpookCorners: {Vector3}? = nil
+local updateConnection: RBXScriptConnection? = nil
+
+function DeanHaunt.initialize()
+	local player = Players.LocalPlayer
+	if not player then return end
+
+	player.CharacterAppearanceLoaded:Connect(function(character)
+		DeanHaunt.startSpookCheck(character)
+	end)
+
+	if player.Character then
+		DeanHaunt.startSpookCheck(player.Character)
+	end
+end
+
+function DeanHaunt.startSpookCheck(character: Model): ()
+	characterToCheck = character
+	updateConnection = RunService.Heartbeat:Connect(DeanHaunt.update)
+end
+
+function DeanHaunt.update(deltaTime: number): ()
+	local character = characterToCheck
+	local humanoidRootPart: BasePart
+	if not character or not character.Parent then
+		if updateConnection then
+			characterToCheck = nil
+			updateConnection:Disconnect()
+		end
+		return
+	else
+		local root = character:FindFirstChild("HumanoidRootPart")
+		if not root or not root:IsA("BasePart") then
+			if updateConnection then
+				characterToCheck = nil
+				updateConnection:Disconnect()
+			end
+			return
+		end
+		humanoidRootPart = root
+	end
+
+	if DeanHaunt.canSpawn() then
+		DeanHaunt.spawnBehindPlayerIfPossible(humanoidRootPart)
+	end
+
+	if activeSpook then
+		local isOnScreen = DeanHaunt.checkIfOnScreen(activeSpook)
+		if isOnScreen then
+			HANGING_AROUND_SOUND:Stop()
+			DeanHaunt.disappear(activeSpook)
+			return
+		end
+		if not HANGING_AROUND_SOUND.IsPlaying then
+			HANGING_AROUND_SOUND.Volume = 0
+			HANGING_AROUND_SOUND:Play()
+		end
+		HANGING_AROUND_SOUND.Volume = math.clamp(
+			HANGING_AROUND_SOUND.Volume + HANGING_AROUND_SOUND_RAMP_SPEED * deltaTime,
+			0,
+			HANGING_AROUND_SOUND_MAX_VOLUME
+		)
+	end
+end
+
+function DeanHaunt.spawnBehindPlayerIfPossible(rootPart: BasePart): ()
+	--warn("Spawned")
+	local cameraCFrame = camera.CFrame
+	local lookVector = cameraCFrame.LookVector
+	local behindVector = -lookVector
+
+	-- flatten to horizontal plane (ignore vertical)
+	local flatVector = Vector3.new(behindVector.X, 0, behindVector.Z).Unit
+	local spawnDistance = rng:NextNumber(MIN_SPAWN_DISTANCE, MAX_SPAWN_DISTANCE)
+
+	-- spawn point starts behind the camera
+	local spawnOrigin = cameraCFrame.Position + flatVector * spawnDistance
+
+	local rayOrigin = spawnOrigin + Vector3.new(0, 50, 0)  -- start high above
+	local rayDirection = Vector3.new(0, -100, 0)           -- cast down
+
+	local player = Players.LocalPlayer
+	local raycastParams = RaycastParams.new()
+	raycastParams.FilterDescendantsInstances = {player.Character :: Model}
+	raycastParams.FilterType = Enum.RaycastFilterType.Exclude
+
+	local raycastResult = workspace:Raycast(rayOrigin, rayDirection, raycastParams)
+	if raycastResult then
+		local spawnPosition = raycastResult.Position
+		local lookAtPlayer = CFrame.lookAt(spawnPosition, rootPart.Position)
+		local spookModel = CHARACTER:Clone()
+		spookModel:PivotTo(lookAtPlayer)
+		
+		local cframe, size = spookModel:GetBoundingBox()
+		activeSpook = spookModel
+		activeSpookCorners = BoundingBox.getCornersFromBoundingBox(cframe, size)
+		spookModel.Parent = workspace
+
+		DeanHaunt.playSound(SPAWN_SOUND, SPAWN_SOUND_MIN_PLAYBACK_SPEED, SPAWN_SOUND_MAX_PLAYBACK_SPEED)
+	end
+end
+
+function DeanHaunt.checkIfOnScreen(spookModel: Model): boolean
+	if not activeSpookCorners then
+		error("ACTIVE_CORNERS_NIL")
+	end
+	local isInView = BoundingBox.isBoundingBoxInViewByViewportCorners(BoundingBox.getViewportCorners(activeSpookCorners, camera))
+	return isInView
+end
+
+function DeanHaunt.playSound(sound: Sound, minPlaySpeed: number?, maxPlaySpeed: number?, dontPlayIfAlr: boolean?): ()
+	if minPlaySpeed and maxPlaySpeed then
+		sound.PlaybackSpeed = rng:NextNumber(minPlaySpeed, maxPlaySpeed)
+	end
+	if sound.IsPlaying and dontPlayIfAlr then
+		return
+	end
+	sound:Play()
+end
+
+function DeanHaunt.canSpawn(): boolean
+	local random = rng:NextNumber(0, 1)
+	local randomChance = random < SPAWN_CHANCE
+	return not activeSpook and randomChance
+end
+
+function DeanHaunt.disappear(spookModel: Model): ()
+	activeSpook = nil
+	activeSpookCorners = nil
+
+	task.spawn(function()
+		task.wait(DESPAWN_SPEED)
+		spookModel:Destroy()
+		DeanHaunt.playSound(DISAPPEAR_SOUND, nil, nil, true)
+	end)
+end
+
+return DeanHaunt
