eb3add334b5b50e90805e753ea6c10288bf3d180
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2025-11-28 21:41:58 +0100

    feat(ui): add indicators

--- src/client/PlayerScripts/Client.client.lua
+++ src/client/PlayerScripts/Client.client.lua
@@ -3,9 +3,11 @@
 local Players = game:GetService("Players")
 local LocalizationService = game:GetService("LocalizationService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 local StarterPlayer = game:GetService("StarterPlayer")
 local TypedRemotes = require(ReplicatedStorage.shared.network.remotes.TypedRemotes)
 local ClientLanguage = require(StarterPlayer.StarterPlayerScripts.client.modules.language.ClientLanguage)
+local IndicatorsRenderer = require(StarterPlayer.StarterPlayerScripts.client.modules.renderer.hud.indicator.IndicatorsRenderer)
 local LocalPlayer = Players.LocalPlayer
 
 -- Localization:
@@ -33,3 +35,7 @@ ClientLanguage.load()
 
 -- So that languages can be loaded properly before anything uses it
 local Objectives = require(StarterPlayer.StarterPlayerScripts.client.modules.ui.objectives.Objectives)
+
+RunService.PreRender:Connect(function(deltaTime)
+	IndicatorsRenderer.update()
+end)
