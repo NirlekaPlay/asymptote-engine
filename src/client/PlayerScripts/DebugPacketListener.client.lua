074dbeb5af4f3f7b407b8a28ca7a0473b032a089
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2026-01-12 21:12:55 +0100

    refactor(network)!: use DebugPacketTypes instead of direct DebugPackets access

--- src/client/PlayerScripts/DebugPacketListener.client.lua
+++ src/client/PlayerScripts/DebugPacketListener.client.lua
@@ -2,12 +2,12 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterPlayer = game:GetService("StarterPlayer")
+local DebugPacketTypes = require(ReplicatedStorage.shared.network.DebugPacketTypes)
 local ComputedVoxelsRenderer = require(StarterPlayer.StarterPlayerScripts.client.modules.renderer.debug.ComputedVoxelsRenderer)
-local DebugPackets = require(ReplicatedStorage.shared.network.DebugPackets)
 local TypedRemotes = require(ReplicatedStorage.shared.network.remotes.TypedRemotes)
 
 TypedRemotes.ClientBoundDynamicDebugDump.OnClientEvent:Connect(function(debugName, payload)
-	if debugName == DebugPackets.Packets.DEBUG_COMPUTED_VOXELS then
+	if debugName == DebugPacketTypes.DEBUG_COMPUTED_VOXELS then
 		-- "Trust me bro"
 		ComputedVoxelsRenderer.visualizeComputedNodes(payload)
 	end
