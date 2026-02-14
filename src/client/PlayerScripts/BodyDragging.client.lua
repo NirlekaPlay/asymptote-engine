d5637e34a2f93dd26156af676253d60bc49d68df
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2026-02-14 19:45:21 +0100

    feat!: add responsive key to drop body

--- src/client/PlayerScripts/BodyDragging.client.lua
+++ src/client/PlayerScripts/BodyDragging.client.lua
@@ -3,7 +3,10 @@
 local ContextActionService = game:GetService("ContextActionService")
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterPlayer = game:GetService("StarterPlayer")
+local InteractionKeysScreen = require(StarterPlayer.StarterPlayerScripts.client.modules.ui.screens.InteractionKeysScreen)
 local TypedRemotes = require(ReplicatedStorage.shared.network.remotes.TypedRemotes)
+local Maid = require(ReplicatedStorage.shared.util.misc.Maid)
 
 local localPlayer = Players.LocalPlayer
 
@@ -24,4 +27,46 @@ ContextActionService:BindAction("ACTION_STOP_CARRYING", function(actionName: str
 	TypedRemotes.ServerboundStopBodyDrag:FireServer()
 
 	return Enum.ContextActionResult.Sink
-end, false, Enum.KeyCode.F)
+end, false, Enum.KeyCode.G)
+
+local function showKey(show: boolean): ()
+	if show then
+		InteractionKeysScreen.showInteractionPair("DROP_BODY", "G", "Drop body")
+	else
+		InteractionKeysScreen.removeInteractionPair("DROP_BODY")
+	end
+end
+
+local function processChar(character: Model): ()
+	local maid = Maid.new()
+	local function onDestroy()
+		maid:doCleaning()
+		showKey(false)
+	end
+
+	maid:giveTask(character.Destroying:Connect(onDestroy))
+	maid:giveTask(character.AncestryChanged:Connect(function()
+		if not character:IsDescendantOf(game) then
+			onDestroy()
+		end
+	end))
+	maid:giveTask(character.ChildAdded:Connect(function(child)
+		if child:IsA("ObjectValue") and child.Name == "CurrentDraggingChar" then
+			maid:giveTask(child.Changed:Connect(function(value)
+				showKey(value ~= nil)
+			end))
+
+			if child.Value ~= nil then
+				showKey(true)
+			end
+		end
+	end))
+end
+
+if localPlayer.Character then
+	processChar(localPlayer.Character)
+end
+
+localPlayer.CharacterAdded:Connect(function(char)
+	processChar(char)
+end)
---
+++ src/client/PlayerScripts/modules/ui/screens/InteractionKeysScreen.lua
@@ -0,0 +1,85 @@
+--!strict
+
+local Players = game:GetService("Players")
+local StarterPlayer = game:GetService("StarterPlayer")
+local TweenService = game:GetService("TweenService")
+local UITextShadow = require(StarterPlayer.StarterPlayerScripts.client.modules.ui.components.UITextShadow)
+
+local localPlayer = Players.LocalPlayer
+local playerGui = localPlayer.PlayerGui
+local ui = playerGui:WaitForChild("Interaction")
+local ui_keySideFrame = ui.Root.KeySide
+local ui_actionSideFrame = ui.Root.NameSide
+
+local REF_KEY = ui_keySideFrame.REF
+local REF_ACTION = ui_actionSideFrame.REF
+local PREFIX = "- "
+local BLACK = Color3.new(0, 0, 0)
+
+local ROOT_OFF_SCREEN_POS = UDim2.new(0.5, 0, -1, 0)
+local ROOT_ON_SCREEN_POS = UDim2.new(0.5, 0, 0, 0)
+local SHOW_TWEEN_DUR = 0.5
+local TWEEN_INFO_IN = TweenInfo.new(
+	SHOW_TWEEN_DUR,
+	Enum.EasingStyle.Quart,
+	Enum.EasingDirection.Out
+)
+
+ui.Root.AnchorPoint = Vector2.new(0.5, 0)
+ui.Root.Position = ROOT_OFF_SCREEN_POS
+
+ui.Enabled = true
+
+local keyPairs: { [string]: { key: Frame, action: Frame } } = {}
+
+--[=[
+	@class InteractionKeysScreen
+]=]
+local InteractionKeysScreen = {}
+
+function InteractionKeysScreen.showInteractionPair(id: string, keyStr: string, actionStr: string): ()
+	InteractionKeysScreen.removeInteractionPair(id)
+
+	keyPairs[id] = InteractionKeysScreen.createNewPair(keyStr, actionStr)
+end
+
+function InteractionKeysScreen.removeInteractionPair(id: string): ()
+	local existing = keyPairs[id]
+	if existing then
+		existing.action:Destroy()
+		existing.key:Destroy()
+		keyPairs[id] = nil
+	end
+end
+
+function InteractionKeysScreen.createNewPair(keyStr: string, actionStr: string): { key: Frame, action: Frame }
+	local keyTextFrame = REF_KEY:Clone()
+	local keyTextLabel = keyTextFrame.REF
+	keyTextLabel.Text = keyStr
+	keyTextLabel.Visible = true
+	keyTextFrame.Parent = ui_keySideFrame
+
+	local keyTextLabel_shadow = UITextShadow.createTextShadow(keyTextLabel, nil, 3, nil, 0.5)
+	keyTextLabel_shadow.BackgroundColor3 = BLACK
+	keyTextLabel_shadow.BackgroundTransparency = 0.5
+
+	local actionTextFrame = REF_ACTION:Clone()
+	local actionTextLabel = actionTextFrame.REF
+	actionTextLabel.Text = PREFIX .. actionStr
+	actionTextLabel.Visible = true
+	actionTextFrame.Parent = ui_actionSideFrame
+
+	local _actionTextLabel_shadow = UITextShadow.createTextShadow(actionTextLabel, nil, 3, nil, 0.5)
+
+	return { key = keyTextFrame, action = actionTextFrame }
+end
+
+REF_KEY.REF.Visible = false
+REF_ACTION.REF.Visible = false
+
+task.spawn(function()
+	task.wait(5)
+	TweenService:Create(ui.Root, TWEEN_INFO_IN, { Position = ROOT_ON_SCREEN_POS }):Play()
+end)
+
+return InteractionKeysScreen
