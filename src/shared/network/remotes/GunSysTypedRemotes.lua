af0698589104cc30708c89a43e3f406b4a186d45
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2026-03-08 13:00:07 +0100

    feat(gunsys): change the remotes to unreliable

--- src/shared/network/remotes/GunSysTypedRemotes.lua
+++ src/shared/network/remotes/GunSysTypedRemotes.lua
@@ -4,13 +4,13 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local BulletTracerPayload = require(ReplicatedStorage.shared.network.payloads.BulletTracerPayload)
 local TypedRemote = require(ReplicatedStorage.shared.thirdparty.TypedRemote)
 
-local _, RE = TypedRemote.parent()
+local _, _, URE = TypedRemote.parent()
 
-type RF<T..., R...> = TypedRemote.Function<T..., R...>
 type RE<T...> = TypedRemote.Event<T...>
+type URE<T...> = TypedRemote.UnreliableEvent<T...>
 
 return {
-	BulletTracer = RE("BulletTracer") :: RE<BulletTracerPayload.BulletTracer>,
-	DropShell = RE("DropShell") :: RE<CFrame>,
-	HitRegister = RE("HitRegister") :: RE<boolean>
+	BulletTracer = URE("BulletTracer") :: URE<BulletTracerPayload.BulletTracer>,
+	DropShell = URE("DropShell") :: URE<CFrame>,
+	HitRegister = URE("HitRegister") :: URE<boolean>
 }
